695731cd278815b3cfb63633db9070e10118e1da
Данил Кудрин <88921658+Danil273@users.noreply.github.com>
2023-03-10 00:02:03 +0500

    Update model_preparation.py

--- model_preparation.py
+++ model_preparation.py
@@ -15,7 +15,6 @@ train_data = []
 for path, dirs, files in os.walk(path):
     for filename in files:      
         train_data.append(pd.read_csv('{0}/{1}'.format(path,filename),index_col=0))
-
 train_data = pd.concat(train_data, axis = 0,ignore_index=True)
 X = np.array(train_data.Time).reshape(-1, 1)
 y = np.array(train_data.Temperature).reshape(-1, 1)
